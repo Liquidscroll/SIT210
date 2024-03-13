0a68dd7927340b27bba8a43f08c8e484c347d07c
Liquidscroll <jojaty@gmail.com>
2024-03-13 13:49:40 +1100

    Added completed task 1.1

--- Task1.1_BlinkName/MorseCode.ino
+++ Task1.1_BlinkName/MorseCode.ino
@@ -1,39 +1,90 @@
-//Define DIT's and DAH's
 
-const unsigned long DIT = 300;
-const unsigned long DAH = 900;
+// Pin definitions.
+const int buttonPin = 2;
 
+// Morse code unit definitions.
+const unsigned long unit = 100;
+const unsigned long DIT = unit;
+const unsigned long DAH = unit * 3;
+const unsigned long space = unit * 7;
 
-void morse_s() {
-  for (int i = 0; i < 3; i++) { //3 short pulses
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(DIT);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(DIT);
-  }
-  
-}
+// Define variables used in interrupt.
+volatile bool buttonPressed = false;
+volatile int currIdx = 0;
 
-void morse_o() {
-  for (int i = 0; i < 3; i++) { //3 long pulses
-    digitalWrite(LED_BUILTIN, HIGH);  
-    delay(DAH);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(DIT);
-  }
-}
+// Hardcoded name to be transmitted.
+char n[] = "jonathan";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  
   pinMode(LED_BUILTIN, OUTPUT);
+  // Init as INPUT_PULLDOWN so button will read 1 when pressed, 0 when unpressed.
+  pinMode(buttonPin, INPUT_PULLDOWN);
+
+  // Attach interrupt for button to appropriate pin.
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPress, CHANGE);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  morse_s();
-  delay(DIT*2);
-  morse_o();
-  delay(DIT*2);
-  morse_s();
-  delay(DIT*7);
+  if(buttonPressed)
+  {
+    morseOut(n[currIdx]);
+    
+    if(currIdx > strlen(n))
+    {
+      // If word is finished transmitted then wait for the space between words
+      // Then reset buttonPressed flag and the current index.
+      delay(space);
+      buttonPressed = false;
+      currIdx = 0;
+    }
+    else
+    {
+      // We have no finished transmitting word, so we increment to next index.
+      currIdx++;
+    }
+  }
+}
+
+void buttonPress()
+{
+  // When button is pressed, reset current index to start and deluminate LED.
+  buttonPressed = true;
+  currIdx = 0;
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void morseOut(char letter)
+{
+  switch(letter)
+  {
+    case 'a': dit(); dah(); break;
+    case 'h': dit(); dit(); dit(); dit(); break;
+    case 'j': dit(); dah(); dah(); dah(); break;
+    case 'n': dah(); dit(); break;
+    case 'o': dah(); dah(); dah();
+    case 't': dah();
+    default: break;
+  }
+  delay(DAH);
+}
+
+
+void dit()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(DIT);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(DIT);
+}
+
+void dah()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(DAH);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(DIT);
 }
